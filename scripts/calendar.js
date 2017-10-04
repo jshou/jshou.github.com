2b8d6465435ce99c6536696a013999d0161d719f
jshou <joshuahou@gmail.com>
2017-10-04 12:51:24 -0700

    render with react

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -1,31 +1,64 @@
-var moment = require('moment');
-var Mustache = require('mustache');
+import React from 'react';
+import moment from 'moment';
+
 var $ = require('jquery');
 
-var template = $('#gig').text();
 
-var mykey = 'AIzaSyCA9pV8ZJNnG2Gmerj71DF30noN8DTiQ9c';
-var calendarid = 'nr5jftdjm9p0lg4pigi0dsld6c@group.calendar.google.com';
+class GigCalendar extends React.Component {
+  constructor(props) {
+    super(props);
 
-var options = {
-  key: mykey,
-  maxResults: 20,
-  orderBy: 'startTime',
-  singleEvents: true,
-  timeMin: (new Date()).toISOString()
-};
+    var mykey = 'AIzaSyCA9pV8ZJNnG2Gmerj71DF30noN8DTiQ9c';
+    var calendarid = 'nr5jftdjm9p0lg4pigi0dsld6c@group.calendar.google.com';
 
-$.get('https://www.googleapis.com/calendar/v3/calendars/' + calendarid + '/events?'+ $.param(options), function(data) {
-  for (var i = 0; i < data.items.length; i++) {
-    var date =  moment(data.items[i].start.dateTime).format('dddd, MMM Do YYYY');
-    var start = moment(data.items[i].start.dateTime).format('h:mmA');
-    var end = moment(data.items[i].end.dateTime).format('h:mmA');
-    var band = data.items[i].summary;
-    var venue = data.items[i].location;
-    var venueMap = "https://www.google.com/maps/search/?api=1&query=" + encodeURIComponent(venue);
+    var options = {
+      key: mykey,
+      maxResults: 20,
+      orderBy: 'startTime',
+      singleEvents: true,
+      timeMin: (new Date()).toISOString()
+    };
 
-    var rendered = Mustache.render(template, {date: date, start: start, end: end, band: band, venueMap: venueMap});
-    $('#gigs ul').append(rendered);
+    this.state = {gigs: []};
+    var that = this;
+
+    $.get('https://www.googleapis.com/calendar/v3/calendars/' + calendarid + '/events?'+ $.param(options), function(data) {
+      for (var i = 0; i < data.items.length; i++) {
+        var date =  moment(data.items[i].start.dateTime).format('dddd, MMM Do YYYY');
+        var start = moment(data.items[i].start.dateTime).format('h:mmA');
+        var end = moment(data.items[i].end.dateTime).format('h:mmA');
+        var band = data.items[i].summary;
+        var venue = data.items[i].location;
+        var venueMap = "https://www.google.com/maps/search/?api=1&query=" + encodeURIComponent(venue);
+
+        var gig = <Gig key={i} date={date} start={start} end={end} band={band} venueMap={venueMap}/>;
+        that.setState({gigs: that.state.gigs.concat([gig])});
+      }
+
+    });
   }
 
-});
+  render() {
+    return (
+      <div>
+        <h4>Gigs</h4>
+        <ul>
+          { this.state.gigs }
+        </ul>
+      </div>
+    );
+  }
+}
+
+class Gig extends React.Component {
+  render() {
+    return (
+      <li>
+        <div className="date">{ this.props.date }, { this.props.start } - { this.props.end }</div>
+        <div className="event">{ this.props.band } (<a href="{ this.props.venueMap }">map</a>)</div>
+      </li>
+    );
+  }
+}
+
+export default GigCalendar;
---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import GigCalendar from './calendar';
+
+render(<GigCalendar />, document.getElementById('gigs'));
